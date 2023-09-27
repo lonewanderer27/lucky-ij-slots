443da9a512be9f2bfc5dd35c48acb86ee55875d5
lonewanderer27 <adrianejames03@gmail.com>
2023-09-28 04:07:28 +0800

    remove unnecessary codes & files

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+import './main.css';
+
+import App from './App';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
